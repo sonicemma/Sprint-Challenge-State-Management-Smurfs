ad32e209ff9577c2a2361d3511fae88ce58b2448
emma <sonicjane2020@gmail.com>
2020-03-20 08:27:49 -0700

    coded the axios call and useState

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,29 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-      </div>
-    );
-  }
+
+
+
+const App = () => {
+
+  const [smurfs, setSmurfs] = useState([]);
+
+  // todo make it so you can add a smurf to api
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(res => {
+        console.log(res.data)
+        setSmurfs(res.data)
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+    </div>
+  );
 }
 
 export default App;
